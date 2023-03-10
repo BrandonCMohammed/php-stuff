856ee91ad6c2f336cb31e749fd4e1e35ae5a41ed
BrandonCMohammed <64518176+BrandonCMohammed@users.noreply.github.com>
2023-03-10 14:15:04 -0400

    php stuff

---
+++ forms.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action = "hello.php" method = "post">
+
+        <div>
+            <label for = "fname" >What is your first name? </label>
+            <input name = "fname" type = "text" id = "fname" placeholder="First name">
+        </div>
+
+        <div>
+            <label for = "lname" >What is your last name? </label>
+            <input name = "lname" type = "text" id = "lname" placeholder="Last name">
+        </div>
+
+        <div>
+            <input type ="submit"
+        </div>
+    </form>
+</body>
+</html>
---
+++ hello.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $myvar = "This is a variable";
+        
+        echo "Welcome " . $_POST["fname"] . " " . $_POST["lname"];
+        
+
+        
+    ?>
+</body>
+</html>
